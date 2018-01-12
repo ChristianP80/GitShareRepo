b0821ed6b0f5eb264b5fb39a439873b68cef90f9
christianpersson <chrissp_@hotmail.com>
2018-01-12 14:39:37 +0100

    Initial Commit

---
+++ GitTrainingShareRepo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTrainingShareRepo
+//
+//  Created by Christian Persson on 2018-01-12.
+//  Copyright © 2018 Christian Persson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTrainingShareRepo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTrainingShareRepo
+//
+//  Created by Christian Persson on 2018-01-12.
+//  Copyright © 2018 Christian Persson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
